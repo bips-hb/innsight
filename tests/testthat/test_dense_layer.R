75bd4bcf5f0431a1a5db9d8af475fbaceee5be85
nkoenen <niklas.koenen@gmail.com>
2021-06-25 15:52:11 +0200

    Update test for dense_layer

--- tests/testthat/test_dense_layer.R
+++ tests/testthat/test_dense_layer.R
@@ -1,120 +1,110 @@
-test_that("Test for Dense-Layers",{
+test_that("Test LRP for Dense-Layers",{
   library(torch)
+  batch_size = 10
+  for(dim_in in c(1,2,10,20)){
+    for(dim_out in c(1,3,10,30)){
+      for(activation_name in c("relu","tanh","softplus")){
 
-  #
-  # Define Dense-Layer
-  #
-  W <- matrix(rnorm(15), nrow = 3, ncol = 5)
-  b <- rnorm(3) * 0
-  dense <- dense_layer(weight = W, bias = b, "relu")
+        #
+        # Test for dtype = "float"
+        #
 
-  #
-  # ------------------------ Test forward method -------------------------------
-  #
-  input <- torch_randn(20,5, dtype = torch_float())
-  y <- dense(input)
-  y_true <- dense$activation_f(t(W %*% t(as_array(input)) + b))
+        W <- torch_randn(dim_out,dim_in)
+        b <- torch_randn(dim_out)
+        dense <- dense_layer(weight = W, bias = b, activation_name,dtype = "float")
 
-  expect_true(as_array(mean(mean(abs(y- y_true), dim=2))) < 1e-5)
+        input <- torch_randn(batch_size, dim_in, dtype = torch_float())
+        input_ref <- torch_randn(1, dim_in, dtype = torch_float())
 
-  #
-  # ------------------------ Test update method --------------------------------
-  #
-  input_ref <- torch_randn(1,5, dtype = torch_float())
-  y <- dense$update_ref(input_ref)
-  y_true <- dense$activation_f(t(W %*% t(as_array(input_ref)) + b))
+        #
+        # Test forward
+        #
+        y <- dense(input)
+        expect_equal(dim(y), c(batch_size, dim_out))
 
-  expect_true(as_array(mean(abs(y- y_true))) < 1e-5)
+        #
+        # Test update_ref
+        #
+        y <- dense$update_ref(input_ref)
+        expect_equal(dim(y), c(1, dim_out))
 
-  #
-  # ------------------- Test get_input_relevances method -----------------------
-  #
-  rel <- torch_randn(4,3,2, dtype = torch_float())
-  input <- torch_randn(4,5, dtype = torch_float())
-  dense(input)
+        #
+        # Test get_gradient
+        #
+        input$requires_grad <- TRUE
+        dense(input)
+        y <- dense$preactivation
+        sum(y)$backward()
+        y_true <- input$grad$unsqueeze(3)
+        y <- dense$get_gradient(torch_ones(batch_size, dim_out, 1), W)
+        expect_lt(as_array(mean((y - y_true)^2)), 1e-8)
+        input$requires_grad <- FALSE
 
-  x_in <- input$reshape(c(4,1,5))
-  z_ij <- torch_mul(x_in, dense$W)
-  z_j <- torch_add(torch_sum(z_ij, dim = 3, keepdim = TRUE) ,dense$b$reshape(c(-1,1)))
+        #
+        # Test get_pos_and_neg_outputs
+        #
+        out <- dense$get_pos_and_neg_outputs(input, use_bias = TRUE)
+        expect_equal(dim(out$pos), c(batch_size, dim_out))
+        expect_equal(dim(out$neg), c(batch_size, dim_out))
+        expect_lt(as_array(mean((out$pos + out$neg - dense$preactivation)^2)), 1e-8)
 
-  # Simple rule
-  y <- dense$get_input_relevances(rel)
-  y_true <- torch_matmul(torch_transpose(z_ij / z_j, 3,2), rel)
+        out <- dense$get_pos_and_neg_outputs(input, use_bias = FALSE)
+        expect_equal(dim(out$pos), c(batch_size, dim_out))
+        expect_equal(dim(out$neg), c(batch_size, dim_out))
+        expect_lt(as_array(mean((out$pos + out$neg - dense$preactivation + b)^2)), 1e-8)
 
-  expect_true("torch_tensor" %in% class(y))
-  expect_equal(dim(y), c(4,5,2))
-  expect_true(as_array(mean(mean(abs(y - y_true), dim= c(2,3)))) < 1e-5)
+        #
+        # get_input_relevances
+        #
+        dense$b <- dense$b * 0
+        dense(input)
+        dense$update_ref(input_ref)
 
-  # Epsilon rule (default)
-  y <- dense$get_input_relevances(rel, rule_name = "epsilon")
-  z <- z_j + torch_sgn(z_j) * 0.001
-  y_true <- torch_matmul(torch_transpose(z_ij / z, 3,2), rel)
+        for (model_out in c(1,3)) {
 
-  expect_true("torch_tensor" %in% class(y))
-  expect_equal(dim(y), c(4,5,2))
+          rel <- torch_randn(batch_size, dim_out, model_out)
 
-  # Alpha_beta rule
-  y <- dense$get_input_relevances(rel, rule_name = "alpha_beta")
-  z <- z_j + torch_sgn(z_j) * 0.001
-  y_true <- torch_matmul(torch_transpose(z_ij / z, 3,2), rel)
+          # Simple rule
+          y <- dense$get_input_relevances(rel)
+          expect_equal(dim(y), c(batch_size, dim_in, model_out))
+          rel_in <- sum(y, dim = 2:3)
+          rel_out <- sum(rel, dim = 2:3)
+          expect_lt(as_array(mean((rel_in - rel_out)^2)), 1e-8)
 
-  expect_true("torch_tensor" %in% class(y))
-  expect_equal(dim(y), c(4,5,2))
+          # Epsilon rule
+          y <- dense$get_input_relevances(rel, rule_name = "epsilon")
+          expect_equal(dim(y), c(batch_size, dim_in, model_out))
 
-  #
-  # ------------------ Test get_input_multiplier method ------------------------
-  #
-  mult <- torch_randn(10,3,2, dtype = torch_float())
-  input <- torch_randn(10,5, dtype = torch_float())
-  input_ref <- torch_randn(1,5, dtype = torch_float())
+          # Alpha_Beta rule
+          y <- dense$get_input_relevances(rel, rule_name = "alpha_beta")
+          expect_equal(dim(y), c(batch_size, dim_in, model_out))
+        }
 
-  dense(input)
-  dense$update_ref(input_ref)
+        #
+        # Test get_input_multiplier
+        #
 
-  delta_input <- (input - input_ref)$unsqueeze(3)
-  delta_output <- (dense$output - dense$output_ref)$unsqueeze(3)
+        for (model_out in c(1,3)) {
 
-  y_true <- sum(mult * delta_output)
+          mult <- torch_randn(batch_size, dim_out, model_out)
+          diff_input <- (input - input_ref)$unsqueeze(3)
+          diff_output <- (dense$output - dense$output_ref)$unsqueeze(3)
 
-  # Rescale rule
-  mult_input <- dense$get_input_multiplier(mult)
-  y <- sum(mult_input * delta_input)
+          y_true <- sum(mult * diff_output, dim = 2:3)
 
-  expect_equal(dim(mult_input), c(10,5,2))
-  expect_true(as_array(abs(y - y_true)) < 1e-5)
+          # Rescale rule
+          mult_in <- dense$get_input_multiplier(mult)
+          expect_equal(dim(mult_in), c(batch_size, dim_in, model_out))
+          y <- sum(mult_in * diff_input, sum = 2:3)
+          expect_lt(as_array(mean((y - y_true)^2)), 1e-8)
 
-  # Reveal-Cancel rule
-  mult_input <- dense$get_input_multiplier(mult, rule_name = "reveal_cancel")
-  y <- sum(mult_input * delta_input)
-
-  expect_equal(dim(mult_input), c(10,5,2))
-  expect_true(as_array(abs(y - y_true)) < 1e-5)
-
-
-  #
-  # ------------------ Test get_pos_and_neg_outputs method ---------------------
-  #
-
-  input <- torch_randn(10,5, dtype = torch_float())
-  dense(input)
-  y <- dense$preactivation
-  out <- dense$get_pos_and_neg_outputs(input, use_bias = TRUE)
-
-  expect_equal(dim(y), dim(out$pos))
-  expect_equal(dim(y), dim(out$neg))
-  expect_true(as_array(mean(abs(y - out$pos - out$neg))) < 1e-5)
-
-  #
-  # --------------------------- Test get_gradient ------------------------------
-  #
-
-  output <- torch_randn(10, 3, 2, dtype = torch_float())
-  W <- dense$W
-  grad <- dense$get_gradient(output, W)
-  grad_true <- torch_matmul(W$t(), output)
-
-  expect_equal(dim(grad), c(10, 5, 2))
-  expect_true(as_array(mean(abs(grad - grad_true))) < 1e-5)
-
-
+          # Reveal Cancel rule
+          mult_in <- dense$get_input_multiplier(mult, rule_name = "reveal_cancel")
+          expect_equal(dim(mult_in), c(batch_size, dim_in, model_out))
+          y <- sum(mult_in * diff_input, sum = 2:3)
+          expect_lt(as_array(mean((y - y_true)^2)), 1e-8)
+        }
+      }
+    }
+  }
 })
