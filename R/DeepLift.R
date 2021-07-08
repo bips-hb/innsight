e4c44ed9bd557b53bfdd9aab2d1ebdac743ce9a2
RaphaelB <raphael.baudeu@gmail.com>
2021-07-08 17:29:23 +0200

    Implemented the deeplift and lrp methods

--- R/DeepLift.R
+++ R/DeepLift.R
@@ -7,6 +7,9 @@ DeepLift <- R6::R6Class(
 
     rule_name = NULL,
     x_ref = NULL,
+    dtype = "float",
+    analyzer = NULL,
+    data = NULL,
 
 
     contribution = NULL,
@@ -17,6 +20,26 @@ DeepLift <- R6::R6Class(
                           dtype = "float") {
 
       checkmate::assertClass(analyzer, "Analyzer")
+      
+      self$analyzer <- analyzer
+      self$rule_name <- rule_name
+      self$data <- data
+      
+      # Get the reference input
+      # NULL: create an input vector of zeros
+      
+      
+      self$update_ref(x_ref)
+      self$x_ref <- x_ref
+      self$dtype <- dtype
+
+      
+      
+      self$set_dtype(dtype)
+
+      
+     private$run()
+      
 
       #
       # toDo
@@ -25,8 +48,73 @@ DeepLift <- R6::R6Class(
       # method 'run' for calculating the contributions and save the result in 'contribution'
       #
     },
+    
+    
+    update_ref = function(x_ref, channels_first = TRUE) {
+      
+      if(!inherits(x_ref,"torch_tensor")){
+        if(is.null(x_ref)){
+          x_ref <- torch::torch_unsqueeze(torch::torch_zeros(self$analyzer$input_dim),1)
 
-    set_dtype = function(dtype) {
+        }else{
+          x_ref <- torch::torch_tensor(as.array(x_ref), dtype = torch::torch_float())
+        }
+      }
+      #x_ref <- torch::torch_tensor(as.array(x_ref), dtype = torch::torch_float())
+      if (channels_first == FALSE) {
+        x_ref <- torch::torch_movedim(x_ref, -1,2)
+      }
+      out_ref <- self$analyzer$model$update_ref(x_ref, channels_first)
+      self$analyzer$input_last_ref <- x_ref
+      
+      torch::as_array(out_ref)
+    },
+
+      
+      set_dtype = function(dtype) {
+        
+        
+        if (dtype == "float") {
+          
+          for(i in 1:length(self$analyzer$model$modules_list)){
+
+
+            
+            layer <- self$analyzer$model$modules_list[[i]]
+
+            if("Flatten_Layer" %in% layer$'.classes' == FALSE){
+              
+              self$analyzer$model$modules_list[[i]]$W <- layer$W$to(torch::torch_float())
+              self$analyzer$model$modules_list[[i]]$b <- layer$b$to(torch::torch_float())
+              
+            }
+          }
+        }
+        else if (dtype == "double") {
+          for(i in 1:length(self$analyzer$model$modules_list)){
+            
+            layer <- self$analyzer$model$modules_list[[i]]
+            if("Flatten_Layer" %in% layer$'.classes' == FALSE){
+              
+              
+              self$analyzer$model$modules_list[[i]]$W <- layer$W$to(torch::torch_double())
+              self$analyzer$model$modules_list[[i]]$b <- layer$b$to(torch::torch_double())
+              
+            }
+          }
+        }
+        else {
+          stop(sprintf("Unknown argument for 'dtype' : %s . Use 'float' or 'double' instead"))
+        }
+        
+        
+        self$dtype <- dtype
+        
+        #
+        # toDo
+        #
+        # Set dtype of data and all the model params
+        #
       #
       # toDo
       #
@@ -35,14 +123,28 @@ DeepLift <- R6::R6Class(
     },
 
     change_data = function(data) {
+      
+      self$data <- data
+      private$run()
       #
       # toDo
       #
       # change the data and re-run the method
       #
     },
+    
+    change_ref = function(x_ref,channels_first = TRUE){
+      
+      self$update_ref(x_ref,channels_first)
+      self$x_ref <- x_ref
+      private$run()
+    },
 
     change_rule = function(rule_name, rule_param) {
+      
+      self$rule_name <- rule_name
+      
+      private$run()
       #
       # toDo
       #
@@ -54,6 +156,44 @@ DeepLift <- R6::R6Class(
   private = list(
 
     run = function() {
+
+      
+      output_dim <- self$analyzer$output_dim
+      batch_size <- dim(self$data)[1]
+      
+      
+      rev_layers <- rev(self$analyzer$model$modules_list)
+      last_layer <- rev_layers[[1]]
+
+      
+      mul <- torch::torch_diag_embed(last_layer$output)
+
+      
+
+
+      
+      
+      # other layers
+      for (layer in rev_layers) {
+        if("Flatten_Layer" %in% layer$'.classes'){
+          mul <- layer$reshape_to_input(mul)
+          
+        }else{
+          
+          
+          
+          
+          mul <- layer$get_input_multiplier(mul,self$rule_name)
+
+          
+        }
+        
+      }
+
+      mul
+      
+      
+      
       #
       # toDo
       #
--- R/LRP.R
+++ R/LRP.R
@@ -1,22 +1,37 @@
-
-
 LRP <- R6::R6Class(
   classname = "LRP",
-
+  
   public = list(
-
+    
     rule_name = NULL,
     rule_param = NULL,
-
+    data = NULL,
+    analyzer = NULL,
+    
     relevances = NULL,
-
+    dtype = "float",
+    
     initialize = function(analyzer, data,
                           rule_name = "simple",
                           rule_param = NULL,
                           dtype = "float") {
-
+      
+      
       checkmate::assertClass(analyzer, "Analyzer")
-
+      self$rule_name <- rule_name
+      self$rule_param <- rule_param
+      self$analyzer <- analyzer
+      self$data <- data
+      self$dtype <- dtype
+      
+      
+      #Bit weird , should we first set self$dtype <- dtype?
+      self$set_dtype(dtype)
+      
+      self$analyzer$forward(data,channels_first = FALSE)
+      
+      private$run()
+      
       #
       # toDo
       #
@@ -24,24 +39,71 @@ LRP <- R6::R6Class(
       # method 'run' for calculating the relevances and save the result in 'relevances'
       #
     },
-
+    
     set_dtype = function(dtype) {
+
+      
+     if (dtype == "float") {
+
+       
+       for(i in 1:length(self$analyzer$model$modules_list)){
+         
+         layer <- self$analyzer$model$modules_list[[i]]
+         if("Flatten_Layer" %in% layer$'.classes' == FALSE){
+
+
+          self$analyzer$model$modules_list[[i]]$W <- layer$W$to(torch::torch_float())
+          self$analyzer$model$modules_list[[i]]$b <- layer$b$to(torch::torch_float())
+
+         }
+       }
+      }
+      else if (dtype == "double") {
+        for(i in 1:length(self$analyzer$model$modules_list)){
+          
+          layer <- self$analyzer$model$modules_list[[i]]
+          if("Flatten_Layer" %in% layer$'.classes' == FALSE){
+            
+            
+            self$analyzer$model$modules_list[[i]]$W <- layer$W$to(torch::torch_double())
+            self$analyzer$model$modules_list[[i]]$b <- layer$b$to(torch::torch_double())
+            
+          }
+        }
+       }
+       else {
+        stop(sprintf("Unknown argument for 'dtype' : %s . Use 'float' or 'double' instead"))
+       }
+      
+
+      self$dtype <- dtype
+      
       #
       # toDo
       #
       # Set dtype of data and all the model params
       #
     },
-
+    
     change_data = function(data) {
+      
+      self$data <- data
+      
+      private$run()
       #
       # toDo
       #
       # change the data and re-run the method
       #
     },
-
+    
     change_rule = function(rule_name, rule_param) {
+      
+      self$rule_name <- rule_name
+      self$rule_param <- rule_param
+      
+      private$run()
+      
       #
       # toDo
       #
@@ -49,15 +111,72 @@ LRP <- R6::R6Class(
       #
     }
   ),
-
+  
   private = list(
-
+    
+    #'@title Get relevances relative to model input
+    #'@name run
+    #'@description
+    #'This function passes the relevance through each layer of the network and outputs the 
+    #'relevance relative to the inputs
+    #'@return
+    #'Returns the input relevances, of dimension \emph{(batch_size,model_input,model_output)}
+    
     run = function() {
+      
+
+
+      output_dim <- self$analyzer$output_dim
+      batch_size <- dim(self$data)[1]
+      
+      
+      rev_layers <- rev(self$analyzer$model$modules_list)
+      last_layer <- rev_layers[[1]]
+      
+      out_last <- last_layer$output
+      print("rel")
+      print(last_layer$output)
+
+
+
+        rel <- torch_diag_embed(last_layer$output)
+        print(rel)
+
+        
+        # other layers
+      for (layer in rev_layers) {
+        if("Flatten_Layer" %in% layer$'.classes'){
+          rel <- layer$reshape_to_input(rel)
+          
+        }else{
+          
+
+          
+  
+        rel <- layer$get_input_relevances(rel,self$rule_name,self$rule_param)
+        l <- length(layer$input_dim)
+        s <- rel$sum(2:(1+l))
+        #print(dim(s))
+
+       # print(dim(out_last))
+        print(s-out_last)
+
+        }
+
+      }
+
+      rel
+      sum_rel <- rel$sum(c(2,3))
+
+     print(sum_rel-out_last)
+
+
       #
       # toDo
       #
       # Apply method LRP
       #
+      #}
     }
   )
 )
