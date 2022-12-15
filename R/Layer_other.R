0f95a8851b0cbc6b0ba65ad87febf08eb3a9bfe8
nkoenen <niklas.koenen@gmail.com>
2022-12-15 15:46:01 +0100

    Add stabilizer

--- R/Layer_other.R
+++ R/Layer_other.R
@@ -35,6 +35,17 @@ OtherLayer <- nn_module(
   },
 
   set_dtype = function(dtype) {
+  },
+
+  get_stabilizer = function() {
+    # Get stabilizer
+    if (self$dtype == "float") {
+      eps <- 1e-6 # a bit larger than torch_finfo(torch_float())$eps
+    } else {
+      eps <- 1e-15 # a bit larger than torch_finfo(torch_double())$eps
+    }
+
+    eps
   }
 )
 
@@ -220,7 +231,7 @@ batchnorm_layer <- nn_module(
 
   get_input_relevances = function(rel_output,
                                   rule_name = "simple",
-                                  rule_param = NULL) {
+                                  rule_param = NULL, ...) {
     if (is.null(rule_param)) {
       if (rule_name == "epsilon") {
         rule_param <- 0.001
@@ -229,20 +240,21 @@ batchnorm_layer <- nn_module(
       }
     }
     new_shape <- c(1, self$num_features, rep(1, length(self$input_dim) - 1))
+    eps <- self$get_stabilizer()
 
     if (rule_name == "simple") {
       z <- self$output
       reshaped_gamma <- torch_tensor(self$gamma)$reshape(new_shape)
       reshaped_run_var <- torch_tensor(self$run_var)$reshape(new_shape)
       weight <- reshaped_gamma / (reshaped_run_var + self$eps)^0.5
-      z <- z #+ (z == 0) + 1e-6
+      z <- z + torch_sgn(z) + eps + z$eq(0.0) * eps
       rel <- (weight * self$input / z)$unsqueeze(-1) * rel_output
     } else if (rule_name == "epsilon") {
       z <- self$output
       reshaped_gamma <- torch_tensor(self$gamma)$reshape(new_shape)
       reshaped_run_var <- torch_tensor(self$run_var)$reshape(new_shape)
       weight <- reshaped_gamma / (reshaped_run_var + self$eps)^0.5
-      z <- z + rule_param * (torch_sgn(z) + (z == 0))
+      z <- z + rule_param * torch_sgn(z) + eps *  z$eq(0.0)
       rel <- (weight * self$input / z)$unsqueeze(-1) * rel_output
     } else if (rule_name == "alpha_beta") {
       fact <- torch_tensor(self$gamma / (self$run_var + self$eps)^0.5)$reshape(new_shape)
@@ -250,9 +262,9 @@ batchnorm_layer <- nn_module(
                              (self$run_var + self$eps)^0.5 + self$beta)$reshape(new_shape)
 
       z_plus <- torch_maximum(fact * self$input, 0.0)
-      z_plus <- z_plus + torch_eq(z_plus, 0) * 1e-6
+      z_plus <- z_plus + torch_sgn(z_plus) * eps + torch_eq(z_plus, 0) * eps
       z_minus <- torch_minimum(fact * self$input, 0.0)
-      z_minus <- z_minus - torch_eq(z_minus, 0) * 1e-6
+      z_minus <- z_minus + torch_sgn(z_minus) * eps - torch_eq(z_minus, 0) * eps
 
       rel <- rule_param * z_plus / (z_plus + torch_maximum(bias, 0.0)) +
                 (1 - rule_param) * z_minus / (z_minus + torch_minimum(bias, 0.0))
@@ -270,8 +282,9 @@ batchnorm_layer <- nn_module(
     new_shape <- c(1, self$num_features, rep(1, length(self$input_dim) - 1))
     reshaped_gamma <- torch_tensor(self$gamma)$reshape(new_shape)
     reshaped_run_var <- torch_tensor(self$run_var)$reshape(new_shape)
+    fac <- (reshaped_run_var + self$eps)^0.5
 
-    (reshaped_gamma / (reshaped_run_var + self$eps)^0.5)$unsqueeze(-1) * grad_out
+    (reshaped_gamma / fac)$unsqueeze(-1) * grad_out
   }
 )
 
@@ -409,10 +422,12 @@ activation_layer <- nn_module(
   },
 
   get_input_multiplier = function(mult_output, ...) {
+    eps <- self$get_stabilizer()
     delta_input <- self$input - self$input_ref
     delta_output <- self$output - self$output_ref
 
-    mult_output * (delta_output / delta_input)$unsqueeze(-1)
+    mult_output *
+      (delta_output / (delta_input + delta_input$eq(0.0) * eps))$unsqueeze(-1)
   }
 )
 
