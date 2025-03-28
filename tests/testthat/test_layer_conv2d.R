d02733dab7476215dee3166229112977c3298a59
nkoenen <niklas.koenen@gmail.com>
2025-03-28 08:45:59 +0100

    skip test with functional convolution using dilation and double precision tensors on Windows

--- tests/testthat/test_layer_conv2d.R
+++ tests/testthat/test_layer_conv2d.R
@@ -164,6 +164,9 @@ test_that("Test get_pos_and_neg_outputs and get_gradient", {
   #------------------------- Dilation --------------------------
   #
 
+  # https://github.com/pytorch/pytorch/issues/141221
+  skip_on_os("windows")
+
   input <- torch_randn(batch_size, in_channels, in_height, in_width,
     dtype = torch_double(),
     requires_grad = TRUE
